3812df81b02f56b176a76daed6712a5e5205272c
Angelo Luppino <afl1211@gmail.com>
2016-04-10 18:06:29 -0400

    Fix home page submit functionality

--- public/app/components/home/homeController.js
+++ public/app/components/home/homeController.js
@@ -1,9 +1,13 @@
-thor.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
+thor.controller('homeController', ['$scope', '$location', 'cityService', function($scope, $location, cityService) {
 
 	$scope.city = cityService.city;
 
 	$scope.$watch('city', function() {
 		cityService.city = $scope.city;
 	});
+
+	$scope.submit = function() {
+		$location.path('/forecast');
+	};
 
 }]);
